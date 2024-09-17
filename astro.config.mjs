29ea00333b4208756d524ba97eaed679ad64e5e7
Michael Nichols <mnichols@tamu.edu>
2024-09-17 15:40:29 -0500

    astro party

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "hybrid",
+    integrations: [react(), db()],
+    experimental: {
+        serverIslands: true,
+    },
+});
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+export const collections = {
+    holidays: defineCollection({
+        type: 'content',
+        schema: z.object({
+            title: z.string(),
+            image: z.object({
+                src: z.string().url(),
+                alt: z.string(),
+                credit: z.string()
+            }),
+            date: z.date(),
+            tags: z.array(z.string()),
+        }),
+    }),
+}
